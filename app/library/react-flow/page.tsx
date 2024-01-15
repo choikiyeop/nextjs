fa60493adcbb40f302691a50d680059e29e6a3e2
choikiyeop <cky9709@naver.com>
2024-01-15 17:42:21 +0900

    feat: React Flow 기능 구현

--- app/library/react-flow/page.tsx
+++ app/library/react-flow/page.tsx
@@ -1,11 +1,21 @@
 import { Flow } from "@/components/Flow";
 
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 0 }, data: { label: '2' } },
+  { id: '3', position: { x: 0, y: 100 }, data: { label: '3' } },
+]
+
+const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2', label: "Yes" },
+]
+
 export default function ReactFlowPage() {
   
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <div className="border w-1/2 h-1/2 mx-auto">
-        <Flow />
+        <Flow nodeData={initialNodes} edgeData={initialEdges}/>
       </div>
     </div>
   )
--- components/Flow.tsx
+++ components/Flow.tsx
@@ -1,31 +1,80 @@
 'use client'
-import { useCallback, useState } from 'react';
-import ReactFlow, { Connection, Edge, addEdge, useEdgesState, useNodesState } from 'reactflow';
+import { useCallback } from 'react';
+import ReactFlow, { Connection, ConnectionLineType, Edge, Node, Position, addEdge, useEdgesState, useNodesState } from 'reactflow';
+import dagre from "@dagrejs/dagre";
 import 'reactflow/dist/style.css';
 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 200, y: 50 }, data: { label: '2' } },
-  { id: '3', position: { x: 200, y: -50 }, data: { label: '2' } },
-]
+type DirectionType = 'TB' | 'LR';
 
-const initialEdges = [
-  { id: 'e1-2', source: '1', target: '2', label: "Yes" }
-]
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
 
+const NODE_WIDTH = 172;
+const NODE_HEIGHT = 36;
 
-export const Flow = () => {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+const getLayoutedElements = (nodes: Node[], edges: Edge[], direction = 'LR') => {
+  const isHorizontal = direction === 'LR';
+  dagreGraph.setGraph({ rankdir: direction });
 
-  const onConnect = useCallback((connection: Edge | Connection) => {
-    setEdges((eds) => addEdge(connection, eds))
-  }, [setEdges]);
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: NODE_WIDTH, height: NODE_HEIGHT });
+  });
 
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.targetPosition = isHorizontal ? Position.Left : Position.Top;
+    node.sourcePosition = isHorizontal ? Position.Right : Position.Bottom;
+
+    node.position = {
+      x: nodeWithPosition.x - NODE_WIDTH / 2,
+      y: nodeWithPosition.y - NODE_HEIGHT / 2,
+    };
+
+    return node;
+  });
+
+  return { nodes, edges };
+};
+
+interface FlowProps {
+  nodeData: Node[];
+  edgeData: Edge[];
+}
+
+export const Flow = ({ nodeData, edgeData }: FlowProps) => {
+  const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(nodeData, edgeData);
+  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+
+  const onConnect = useCallback((params: Edge | Connection) => {
+    setEdges((eds) =>
+      addEdge({ ...params, type: ConnectionLineType.Bezier, animated: true }, eds)
+    )
+  }, []);
+
+  const onLayout = useCallback((direction: DirectionType) => {
+    const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(nodes, edges, direction);
+
+    setNodes([...layoutedNodes]);
+    setEdges([...layoutedEdges]);
+  }, [nodes, edges]);
 
   return (
-    <ReactFlow nodes={nodes} edges={edges} fitView>
-
-    </ReactFlow>
-  )
-}
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      // onConnect={onConnect}
+      // connectionLineType={ConnectionLineType.Bezier}
+      nodesConnectable={false}
+      fitView
+    />
+  );
+};
