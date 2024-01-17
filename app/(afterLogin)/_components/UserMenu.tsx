98a53d4e2021228ca247e4fb5c558832dc1774fd
choikiyeop <cky9709@naver.com>
2024-01-17 16:05:19 +0900

    feat: Avatar 기능 구현

--- app/(afterLogin)/_components/UserMenu.tsx
+++ app/(afterLogin)/_components/UserMenu.tsx
@@ -1,9 +1,15 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export const UserMenu = () => {
 
   return (
-    <div>
-      유저 드롭다운
+    <div className="flex w-full h-[132px] items-center space-x-2 pl-4 bg-slate-700">
+      <Avatar className="h-8 w-8">
+        <AvatarImage src="https://github.com/shadcn.png" alt="user" />
+        <AvatarFallback>NO</AvatarFallback>
+      </Avatar>
+      <h2 className="text-xs font-semibold text-white">관리자</h2>
+      
     </div>
   )
 }
